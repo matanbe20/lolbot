145e3167a2d90732c4ca3c604b6542d59a33bbe6
Matan Ben Moshe <matanbe18@gmail.com>
2022-01-09 23:30:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const redis = require("redis");
 const redisClient = redis.createClient({
   port: 11411,
   host: "redis-11411.c267.us-east-1-4.ec2.cloud.redislabs.com",
-  password: "GxIQjinpsjKXZbsUilnefR0p9YNOtJoF",
+  password: "password",
 });
 const { promisify } = require("util");
 const { fetchChampions } = require("./repository");
