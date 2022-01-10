a165d54aceae1a81737c23c2483ebe0092830b9d
Matan Ben Moshe <matanbe18@gmail.com>
2022-01-10 09:30:50 +0200

    added error handlers

--- index.js
+++ index.js
@@ -45,3 +45,18 @@ client.on("message", async (msg) => {
 });
 
 client.login(token);
+
+
+process.on('uncaughtException', (error, origin) => {
+    console.log('----- Uncaught exception -----')
+    console.log(error)
+    console.log('----- Exception origin -----')
+    console.log(origin)
+})
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.log('----- Unhandled Rejection at -----')
+    console.log(promise)
+    console.log('----- Reason -----')
+    console.log(reason)
+})
